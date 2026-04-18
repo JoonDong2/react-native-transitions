6b06f7ad9f39c11f37c78a79a28fbc42ac723158
Joondong <kwonjoondong@gmail.com>
2026-04-18 14:54:04 +0900

    refactor(transitions): use Screen shouldFreeze prop instead of Freeze wrapper

--- src/AnimatedItem.tsx
+++ src/AnimatedItem.tsx
@@ -7,86 +7,109 @@ import { styles } from './styles';
 
 const AnimatedScreen = Animated.createAnimatedComponent(Screen);
 
-export const AnimatedItem = memo(({
-  children,
-  itemIndex,
-  animatedIndex,
-  isActive,
-  offset,
-  animationType,
-  activityState,
-  priority,
-  vertical,
-  containerSize,
-  useNativeScreens = true,
-  freeze = true,
-}: AnimatedItemProps) => {
-  const effectiveIndex = useMemo(() => Animated.add(itemIndex, offset), [itemIndex, offset]);
-  const diff = useMemo(() => Animated.subtract(effectiveIndex, animatedIndex), [effectiveIndex, animatedIndex]);
+export const AnimatedItem = memo(
+  ({
+    children,
+    itemIndex,
+    animatedIndex,
+    isActive,
+    offset,
+    animationType,
+    activityState,
+    priority,
+    vertical,
+    containerSize,
+    useNativeScreens = true,
+    freeze = true,
+  }: AnimatedItemProps) => {
+    const effectiveIndex = useMemo(
+      () => Animated.add(itemIndex, offset),
+      [itemIndex, offset]
+    );
+    const diff = useMemo(
+      () => Animated.subtract(effectiveIndex, animatedIndex),
+      [effectiveIndex, animatedIndex]
+    );
 
-  const animatedStyle = useMemo(() => {
-    if (animationType === 'none') return {};
+    const animatedStyle = useMemo(() => {
+      if (animationType === 'none') return {};
 
-    const style: {
-      opacity?: Animated.AnimatedInterpolation<number>;
-      transform?: { translateX: Animated.AnimatedInterpolation<number>; }[];
-    } | {
-      opacity?: Animated.AnimatedInterpolation<number>;
-      transform?: { translateY: Animated.AnimatedInterpolation<number> }[];
-    } = {};
+      const style:
+        | {
+            opacity?: Animated.AnimatedInterpolation<number>;
+            transform?: {
+              translateX: Animated.AnimatedInterpolation<number>;
+            }[];
+          }
+        | {
+            opacity?: Animated.AnimatedInterpolation<number>;
+            transform?: {
+              translateY: Animated.AnimatedInterpolation<number>;
+            }[];
+          } = {};
 
-    if (animationType === 'fade' || animationType === 'fade-slide') {
-      style.opacity = diff.interpolate({
-        inputRange: [-1, -0.5, 0, 0.5, 1],
-        outputRange: [0, 0.5, 1, 0.5, 0],
-        extrapolate: 'clamp',
-      });
-    }
-
-    if (animationType === 'slide' || animationType === 'fade-slide') {
-      const translate = Animated.multiply(diff, containerSize);
-      if (vertical) {
-        style.transform = [{ translateY: translate }];
-      } else {
-        style.transform = [{ translateX: translate }];
+      if (animationType === 'fade' || animationType === 'fade-slide') {
+        style.opacity = diff.interpolate({
+          inputRange: [-1, -0.5, 0, 0.5, 1],
+          outputRange: [0, 0.5, 1, 0.5, 0],
+          extrapolate: 'clamp',
+        });
       }
+
+      if (animationType === 'slide' || animationType === 'fade-slide') {
+        const translate = Animated.multiply(diff, containerSize);
+        if (vertical) {
+          style.transform = [{ translateY: translate }];
+        } else {
+          style.transform = [{ translateX: translate }];
+        }
+      }
+
+      return style;
+    }, [animationType, diff, containerSize, vertical]);
+
+    const containerStyle = {
+      zIndex: priority,
+      [vertical ? 'height' : 'width']: containerSize,
+    };
+
+    const commonStyle = [
+      !isActive && styles.inactiveItem,
+      animatedStyle,
+      styles.itemContainer,
+      containerStyle,
+    ];
+
+    const childContent = isValidElement(children)
+      ? children
+      : children({ activityState, priority, diff });
+
+    const shouldFreeze = freeze && activityState !== ActivityState.FULL_ACTIVE;
+
+    if (useNativeScreens) {
+      return (
+        <AnimatedScreen
+          shouldFreeze={shouldFreeze}
+          activityState={activityState}
+          style={commonStyle}
+          pointerEvents={
+            activityState === ActivityState.FULL_ACTIVE ? 'auto' : 'none'
+          }
+        >
+          {childContent}
+        </AnimatedScreen>
+      );
     }
 
-    return style;
-  }, [animationType, diff, containerSize, vertical]);
-
-  const containerStyle = {
-    zIndex: priority,
-    [vertical ? 'height' : 'width']: containerSize,
-  };
-
-  const commonStyle = [!isActive && styles.inactiveItem, animatedStyle, styles.itemContainer, containerStyle];
-
-  const childContent = isValidElement(children) ? children : children({ activityState, priority, diff });
-  const frozenContent = (
-    <Freeze freeze={freeze && activityState === ActivityState.INACTIVE}>
-      {childContent}
-    </Freeze>
-  );
-
-  if (useNativeScreens) {
     return (
-      <AnimatedScreen
-        activityState={activityState}
-        style={commonStyle}
-        pointerEvents={activityState === 2 ? 'auto' : 'none'}
-      >
-        {frozenContent}
-      </AnimatedScreen>
+      <Freeze freeze={shouldFreeze}>
+        <Animated.View
+          style={commonStyle}
+          pointerEvents={activityState === 2 ? 'auto' : 'none'}
+        >
+          {childContent}
+        </Animated.View>
+      </Freeze>
     );
   }
-
-  return (
-    <Animated.View
-      style={commonStyle}
-      pointerEvents={activityState === 2 ? 'auto' : 'none'}
-    >
-      {frozenContent}
-    </Animated.View>
-  );
-});
+);
